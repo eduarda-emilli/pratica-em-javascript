43162dd9c6155193e15a1d4095a892a725dac90a
Eduarda Emilli <107655524+eduarda-emilli@users.noreply.github.com>
2023-01-03 15:33:22 -0300

    index atualizado

---
+++ sistema_de_multa.js
@@ -0,0 +1,7 @@
+var vel = 60.5
+console.log(`A sua velocidade atual é ${vel}KM/h`)
+if (vel < 60) {
+    console.log('Você ultrapassou a velocidade permitida. MULTADO')
+} else {
+    console.log('Você está na velocidade ideal. Continue assim.')
+}
